f5422934a507e7d379cbc7b11611a2656456d324
lileoniani <lile.oniani13@gmail.com>
2022-12-09 17:24:36 +0400

    /

---
+++ src/test/java/Google/HomeWorkFour.java
@@ -0,0 +1,34 @@
+package Google;
+
+import org.junit.Test;
+import com.codeborne.selenide.Condition;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class HomeWorkFour extends RegistrationVariables{
+
+    @Test
+    public void fbTest(){
+        open("https://www.facebook.com/");
+        String testEmail = "blah@gmail.com";
+        String pass = "Test@123";
+        regButton.click();
+        firstName.sendKeys("blah");
+        surname.sendKeys("blahblah");
+        email.sendKeys(testEmail);
+        repeatEmail.shouldBe(Condition.visible);
+        repeatEmail.sendKeys(testEmail);
+        newPass.sendKeys(pass);
+        birthDay.selectOption(9);
+        birthMonth.selectOption("Nov");
+        birthYear.selectOption(19);
+        customGenderCheckTrue();
+        customPronoun.selectOption(2);
+        $(byText("Male")).click();
+        customGenderCheckFalse();
+    }
+}
---
+++ src/test/java/Google/RegistrationVariables.java
@@ -0,0 +1,35 @@
+package Google;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class RegistrationVariables {
+    SelenideElement regButton = $(byText("Create New Account"));
+    SelenideElement firstName = $(byAttribute("placeholder", "First name"));
+    SelenideElement surname = $(byAttribute("aria-label", "Surname"));
+    SelenideElement email = $(byAttribute("aria-label", "Mobile number or email address"));
+    SelenideElement repeatEmail = $(byAttribute("aria-label", "Re-enter email address"));
+    SelenideElement newPass = $(byAttribute("aria-label", "New password"));
+    SelenideElement birthDay = $(byAttribute("title", "Day"));
+    SelenideElement birthMonth = $(byAttribute("title", "Month"));
+    SelenideElement birthYear = $(byAttribute("title", "Year"));
+    SelenideElement customGender = $(byText("Custom"));
+    SelenideElement customPronoun = $(byAttribute("aria-label", "Select your pronoun"));
+    SelenideElement genderOptional = $(byAttribute("aria-label", "Gender (optional)"));
+
+
+    public void customGenderCheckTrue() {
+        $(byText("Custom")).click();
+        customPronoun.shouldBe(Condition.visible);
+        genderOptional.shouldBe(Condition.visible);
+    }
+
+    public void customGenderCheckFalse() {
+        customPronoun.shouldNotBe(Condition.visible);
+        genderOptional.shouldNotBe(Condition.visible);
+    }
+}
